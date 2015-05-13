144d4237dfa3eeb81cccef1952f0bdc519bea987
holmes802010 <lilunho@gmail.com>
2015-05-13 10:39:48 -0400

    fixed python2 division bug

---
+++ temp_functions.py
@@ -0,0 +1,11 @@
+def k_to_c(temp):
+    return temp - 273.15
+
+def f_to_k(Temp): 
+    return ((temp-32)*(5/9)) + 273.15
+
+def f_to_c(temp):
+    temp_k = f_to_k(temp)
+    result = k_to_c(temp)
+    return result
+
